3a4a5f0f5396ce3c0cec481258e13bbbbd208ed7
ControlFlow Graph <controlflowgraph@gmail.com>
2022-10-31 11:03:40 +0100

    add MethodBuilder base

---
+++ src/jbapi/MethodBuilder.java
@@ -0,0 +1,14 @@
+package jbapi;
+
+public class MethodBuilder
+{
+    public static MethodBuilder builder()
+    {
+        return new MethodBuilder();
+    }
+
+    private MethodBuilder()
+    {
+
+    }
+}
