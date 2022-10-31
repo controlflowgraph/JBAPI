e76256d493ed6deb0bf02d897354e905b0ab60ad
ControlFlow Graph <controlflowgraph@gmail.com>
2022-10-31 10:47:11 +0100

    add version attribute to ClassBuilder

--- src/jbapi/ClassBuilder.java
+++ src/jbapi/ClassBuilder.java
@@ -7,11 +7,18 @@ public class ClassBuilder
         return new ClassBuilder();
     }
 
+    private int version;
     private String name;
 
     private ClassBuilder()
     {
 
+    }
+
+    public ClassBuilder version(int version)
+    {
+        this.version = version;
+        return this;
     }
 
     public ClassBuilder name(String name)
