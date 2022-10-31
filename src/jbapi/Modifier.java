59800294f6f576bc0f4158430a27ba4899bd0b10
ControlFlow Graph <controlflowgraph@gmail.com>
2022-10-31 11:43:18 +0100

    add associated values to Modifier

--- src/jbapi/Modifier.java
+++ src/jbapi/Modifier.java
@@ -1,11 +1,20 @@
 package jbapi;
 
+import static org.objectweb.asm.Opcodes.*;
+
 public enum Modifier
 {
-    PUBLIC,
-    PRIVATE,
-    PROTECTED,
-    STATIC,
-    FINAL,
+    PUBLIC(ACC_PUBLIC),
+    PRIVATE(ACC_PRIVATE),
+    PROTECTED(ACC_PROTECTED),
+    STATIC(ACC_STATIC),
+    FINAL(ACC_FINAL),
     ;
+
+    private final int value;
+
+    Modifier(int value)
+    {
+        this.value = value;
+    }
 }
