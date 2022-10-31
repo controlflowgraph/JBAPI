85d912104ba9ba006efc59bedd9cc861159807e1
ControlFlow Graph <controlflowgraph@gmail.com>
2022-10-31 10:44:48 +0100

    add ClassBuilder base

---
+++ src/jbapi/ClassBuilder.java
@@ -0,0 +1,14 @@
+package jbapi;
+
+public class ClassBuilder
+{
+    public static ClassBuilder builder()
+    {
+        return new ClassBuilder();
+    }
+
+    private ClassBuilder()
+    {
+
+    }
+}
