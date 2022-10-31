fe7200aee5d4f6afaf4d6110e30c4d7da32ec627
ControlFlow Graph <controlflowgraph@gmail.com>
2022-10-31 11:24:27 +0100

    add instructions attribute to CodeBuilder

--- src/jbapi/CodeBuilder.java
+++ src/jbapi/CodeBuilder.java
@@ -1,5 +1,8 @@
 package jbapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CodeBuilder
 {
     public static CodeBuilder builder()
@@ -7,8 +10,22 @@ public class CodeBuilder
         return new CodeBuilder();
     }
 
+    private final List<Instruction> instructions = new ArrayList<>();
+
     private CodeBuilder()
     {
 
+    }
+
+    private CodeBuilder add(String mnemonic)
+    {
+        this.instructions.add(new Instruction(mnemonic));
+        return this;
+    }
+
+    private CodeBuilder add(String mnemonic, Object... values)
+    {
+        this.instructions.add(new Instruction(mnemonic, values));
+        return this;
     }
 }
