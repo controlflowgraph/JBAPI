7372f8559d251cd184a1b595ce1c3dd6fee3ede4
ControlFlow Graph <controlflowgraph@gmail.com>
2022-11-09 12:05:44 +0100

    add fsub instruction implementation to CodeBuilder

--- src/jbapi/CodeBuilder.java
+++ src/jbapi/CodeBuilder.java
@@ -236,6 +236,7 @@ public class CodeBuilder
             case "mul-integer" -> method.visitInsn(IMUL);
             case "div-integer" -> method.visitInsn(IDIV);
             case "add-float" -> method.visitInsn(FADD);
+            case "sub-float" -> method.visitInsn(FSUB);
             case "return" -> method.visitInsn(RETURN);
             case "aload" -> method.visitVarInsn(ALOAD, i.arguments().getAs(Integer.class, 0));
             case "define-label" -> {
